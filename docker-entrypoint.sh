7de76943e6446e6342b587936bc5892014212cbe
adeleye1416 <64331157+adeleye1416@users.noreply.github.com>
2023-06-16 17:52:40 -0500

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+addgroup -g $GID -S $FTP_USER
+
+adduser -D -G $FTP_USER -h /home/$FTP_USER -s /bin/false -u $UID $FTP_USER
+
+mkdir -p /home/$FTP_USER
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd
+
+touch /var/log/vsftpd.log /var/log/xferlog
+tail -f /var/log/vsftpd.log | tee /dev/stdout &
+tail -f /var/log/xferlog | tee /dev/stdout &
+
+sed -i "s/^pasv_address=[0-9.]\+/pasv_address=$EXTERNAL_IP/" /etc/vsftpd.conf
+sed -i "s/^pasv_min_port=[0-9.]\+/pasv_min_port=$PASSIVE_MIN_PORT/" /etc/vsftpd.conf
+sed -i "s/^pasv_max_port=[0-9.]\+/pasv_max_port=$PASSIVE_MAX_PORT/" /etc/vsftpd.conf
+
+
+exec "$@"
